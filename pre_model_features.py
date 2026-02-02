397e780d103dc7719eb0f4358bea6b6829b139fa
gawainxu <xgawain@gmail.com>
2026-02-02 21:19:50 +0100

    cosine detection added

--- pre_model_features.py
+++ pre_model_features.py
@@ -38,7 +38,7 @@ def parse_option():
     parser.add_argument('--layers_to_see', type=str, default="features.40")
 
     parser.add_argument('--model', type=str, default='resnet18',
-                        choices=["resnet18", "resnet50"])
+                        choices=["resnet18", "resnet50", "vgg16"])
     parser.add_argument("--dataset", type=str, default="imagenet50")
     parser.add_argument("--batch_size", type=int, default=1)
     parser.add_argument("--outliers",  action="store_true", help="if the outlier data")
