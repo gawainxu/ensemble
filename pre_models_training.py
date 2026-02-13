894100faa3423eca8e6412b3cb0ff12f3753cb26
gawainxu <xgawain@gmail.com>
2026-02-13 18:01:53 +0100

    vit for cifar updated

--- pre_models_training.py
+++ pre_models_training.py
@@ -14,6 +14,7 @@ from torch.utils.data import DataLoader
 from networks.resnet_big import SupCEResNet
 from networks.vgg import vgg16, vgg16_bn
 from networks.ViT import ViT, get_b16_config_cifar, get_b16_config
+from networks.ViT_cifar import ViT_cifar
 from util import AverageMeter
 
 image_size_mapping = {"cifar100": 32, "imagenet50": 224}
@@ -123,11 +124,12 @@ def load_model(opt):
     elif "vit" in opt.model:
         if "cifar" in opt.dataset:
             configs = get_b16_config_cifar()
+            model = ViT_cifar()
         elif "imagenet" in opt.dataset:
             configs = get_b16_config()
-        model = ViT(image_size=opt.image_size, patch_size=configs.patch_size, num_classes=opt.num_classes,
-                    embedding_dim=configs.embed_dim, depth=configs.depth, heads=configs.num_heads, mlp_dim=configs.hidden_dim,
-                    dim_head = configs.head_dim, dropout = configs.dropout, emb_dropout = configs.emb_dropout)
+            model = ViT(image_size=opt.image_size, patch_size=configs.patch_size, num_classes=opt.num_classes,
+                        embedding_dim=configs.embed_dim, depth=configs.depth, heads=configs.num_heads, mlp_dim=configs.hidden_dim,
+                        dim_head = configs.head_dim, dropout = configs.dropout, emb_dropout = configs.emb_dropout)
 
     criterion = torch.nn.CrossEntropyLoss()
 
