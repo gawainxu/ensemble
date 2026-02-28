a971c59da8f1a6fdcc2996f6e86c5d601ea3e256
gawainxu <xgawain@gmail.com>
2026-02-28 20:21:15 +0100

    reshape file name added

--- pre_model_features.py
+++ pre_model_features.py
@@ -263,8 +263,7 @@ def normalFeatureReading_hook(model, opt, data_loader):
 
     # https://zhuanlan.zhihu.com/p/87853615
     for name, module in model.named_modules():
-        #print(name)
-        print("layers_to_see", opt.layers_to_see)
+        print(name)
         if name == opt.layers_to_see:
             handle = module.register_forward_hook(get_activation(name))
 
