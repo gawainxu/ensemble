ca1d22ff54745f51324039ad568c497865b14fe0
gawainxu <xgawain@gmail.com>
2026-09-01 23:42:55 +0200

    new train cifar

--- train_cifar.sh
+++ train_cifar.sh
@@ -1,2 +1,1 @@
-python3 main_ce.py --batch_size 128 --lr 1e-2 --dataset "cifar100_marco" --model "resnet18" --lr_decay_epochs "100,120" --epochs 300 --lr_decay_rate 0.01 --trail 0
-
+python3 main_ce.py --batch_size 128 --learning_rate 1e-2 --dataset "cifar100_marco" --model "resnet18" --lr_decay_epochs "100,120" --epochs 300 --lr_decay_rate 0.01 --trail 0
