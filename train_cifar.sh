1a0629e012422320404213f642ef1c63091cf72f
gawainxu <xgawain@gmail.com>
2026-09-01 23:38:02 +0200

    new train cifar

--- train_cifar.sh
+++ train_cifar.sh
@@ -1,2 +1,2 @@
-python3 main_supcon.py --batch_size 256 --epochs 600 --learning_rate 0.001 --cosine True --lr_decay_rate 0.8 --model "resnet_multi" --datasets "cifar10" --method "SupCon" --trail 0 --temp1 0.005 --temp2 0.01 --temp3 0.05
-python3 main_supcon.py --batch_size 256 --epochs 600 --learning_rate 0.001 --cosine True --lr_decay_rate 0.8 --model "resnet_multi" --datasets "cifar10" --method "SupCon" --trail 2 --temp1 0.005 --temp2 0.01 --temp3 0.05
+python3 main_ce.py --batch_size 128 --lr 1e-2 --dataset "cifar100_marco" --model "resnet18" --lr_decay_epochs "100,120" --epochs 300 --lr_decay_rate 0.01 --trail 0
+
